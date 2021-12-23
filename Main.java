e03d0df62dec81b2baf0cad641edeca3afad614e
Gimin Hong <eksrks0310@gmail.com>
2021-12-24 02:30:57 +0900

    .

--- Main.java
+++ Main.java
@@ -1,4 +1,7 @@
-public class s4_1620 {
+import java.io.*;
+import java.util.*;
+
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
