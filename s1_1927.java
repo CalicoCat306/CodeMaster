23af8016bd92a84e85d682923e4086d0dae572e2
Gimin Hong <eksrks0310@gmail.com>
2021-12-24 07:48:47 +0900

    very fast

--- s1_1927.java
+++ s1_1927.java
@@ -3,33 +3,26 @@ import java.io.*;
 
 public class s1_1927 {
     public static void main(String[] args) throws IOException {
+        PriorityQueue<Integer> priq = new PriorityQueue<>();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        List<Integer> lis = new LinkedList<>();
 
         for (int i = 0; i < N; i++) {
-            int input = Integer.parseInt(br.readLine());
-            if (input == 0) {
-                if (lis.size() == 0) {
+            int inp = Integer.parseInt(br.readLine());
+            if (inp == 0) {
+                if (priq.size() == 0) {
                     bw.write(String.valueOf(0));
                 } else {
-                    bw.write(String.valueOf(find(lis)));
+                    bw.write(String.valueOf(priq.poll()));
                 }
                 bw.newLine();
             } else {
-
+                priq.add(inp);
             }
         }
-    }
-
-    public static int find(List<Integer> lis) {
-        int min = Integer.MAX_VALUE;
-        for (Integer i : lis) {
-            min = Math.max(min, i);
-        }
-        return min;
+        bw.flush();
+        bw.close();
     }
 }
